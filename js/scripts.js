6ae38461e2d61f384121555bff8e3239a22f3676
Vikram <vikrammel@live.com>
2017-12-17 08:05:56 -0800

    refactored code to use dictionaries for prices and times to make it easier to export to spreadsheet (working on that next, will circle back around to debugging non-functioning GETs)

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,87 @@
-//buy prices and times from APIs
-var coinbaseBuy;
-var coinbaseTime;
-var krakenBuy;
-var krakenTime;
-var geminiBuy;
-var geminiTime;
-
-//sell prices and times from APIs
-var zebpaySell;
-var zebpayTime;
-var unocoinSell;
-var unocoinTime;
-var coinsecureSell;
-var coinsecureTime;
+var buyPrices = {}; //dictionary of buy prices
+var sellPrices = {}; //dictionary of sell prices
+var times = {}; //dictionary of server times (or local time substitutes)
 
 //functions to define behavior in the case of success/failure of specific requests
 function coinbaseSuccess(responseData){
-    coinbaseBuy = "$" + responseData.ask;
-    coinbaseTime = responseData.time;
+    buyPrices.coinbaseBuy = "$" + responseData.ask;
+    times.coinbaseTime = responseData.time;
 }
+
 function coinbaseError(){
-    coinbaseBuy = "coinbase price GET error";
-    coinbaseTime = "coinbase time GET error";
+    buyPrices.coinbaseBuy = "coinbase price GET error";
+    times.coinbaseTime = "coinbase time GET error";
 }
 
 function krakenPriceSuccess(responseData){
     if (length(responseData.error)>0){
-        krakenBuy = "errors retrieving price from kraken";
+        buyPrices.krakenBuy = "errors retrieving price from kraken";
     }
     else{
-        krakenBuy = "$" + responseData.result.XXBTZUSD.a[0];                   
+        buyPrices.krakenBuy = "$" + responseData.result.XXBTZUSD.a[0];                   
     }
 }
 
 function krakenPriceError(){
-    krakenBuy = "kraken price GET error";
+    buyPrices.krakenBuy = "kraken price GET error";
 }
 
 function krakenTimeSuccess(responseData){
     if (length(responseData.error)>0){
-        krakenTime = "errors retrieving time from kraken";
+        times.krakenTime = "errors retrieving time from kraken";
     }
     else{
-        krakenTime = responseData.result.rfc1123;
+        times.krakenTime = responseData.result.rfc1123;
     }
 }
 
 function krakenTimeError(){
-    krakenTime = "kraken time GET error";
+    times.krakenTime = "kraken time GET error";
 }
 
 function geminiSuccess(responseData){
-    geminiBuy = "$" + responseData.ask;
-    geminiTime = responseData.volume.timestamp;
+    buyPrices.geminiBuy = "$" + responseData.ask;
+    times.geminiTime = responseData.volume.timestamp;
     //TODO: convert geminiTime from unixtime to rfc1123
 }
 
 function geminiError(){
-    geminiBuy = "gemini price GET error";
-    geminiTime = "gemini time GET error";
+    buyPrices.geminiBuy = "gemini price GET error";
+    times.geminiTime = "gemini time GET error";
 }
 
 function zebpaySuccess(responseData){
-    zebpaySell = "Rs." + responseData.sell;
+    sellPrices.zebpaySell = "Rs." + responseData.sell;
     var d = new Date();
-    zebpayTime = d;
+    times.zebpayTime = d;
 }
 
 function zebpayError(){
-    zebpaySell = "zebpay price GET error";
-    zebpayTime = "zebpay time GET error";
+    sellPrices.zebpaySell = "zebpay price GET error";
+    times.zebpayTime = "zebpay time GET error";
 }
 
 function unocoinSuccess(responseData){
-    unocoinSell = "Rs." + responseData;
+    sellPrices.unocoinSell = "Rs." + responseData;
     var d = new Date();
-    unocoinTime = d;
+    times.unocoinTime = d;
 }
 
 function unocoinError(){
-    unocoinSell = "unocoin price GET error";
-    unocoinTime = "unocoin time GET error";
+    sellPrices.unocoinSell = "unocoin price GET error";
+    times.unocoinTime = "unocoin time GET error";
 }
 
 function coinsecureSuccess(responseData){
     //divide rate by 100, last 2 digits are paise
-    coinsecureSell = "Rs." + float(data.message.rate)/100.00;
-    coinsecureTime = data.time;
+    sellPrices.coinsecureSell = "Rs." + float(data.message.rate)/100.00;
+    times.coinsecureTime = data.time;
     //TODO: convert to rfc1123
 }
 
 function coinsecureError(){
-    coinsecureSell = "coinsecure price GET error";
-    coinsecureTime = "coinsecure time GET error";
+    sellPrices.coinsecureSell = "coinsecure price GET error";
+    times.coinsecureTime = "coinsecure time GET error";
 }
 
 function callExchangeApi(type, url, dataType, success, error){
@@ -117,47 +106,48 @@ $(document).ready(function() {
         var coinbaseCall = callExchangeApi('GET', 'https://api.gdax.com/products/BTC-USD/ticker', 
             'json', coinbaseSuccess, coinbaseError);
         coinbaseCall.always(function(){
-            $("#coinbaseBuy").html(coinbaseBuy);
-            $("#coinbaseDateTime").html(coinbaseTime);
+            //write to html objects after call finishes (success or error)
+            $("#coinbaseBuy").html(buyPrices.coinbaseBuy);
+            $("#coinbaseDateTime").html(times.coinbaseTime);
         });
         
         var krakenPriceCall = callExchangeApi('GET', 'https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD', 
             'json', krakenPriceSuccess, krakenPriceError);
         krakenPriceCall.always(function(){
-            $("#krakenBuy").html(krakenBuy);
+            $("#krakenBuy").html(buyPrices.krakenBuy);
             var krakenTimeCall = callExchangeApi('GET', 'https://api.kraken.com/0/public/Time', 
                 'json', krakenTimeSuccess, krakenTimeError);
             krakenTimeCall.always(function(){
-                $("#krakenDateTime").html(krakenTime);
+                $("#krakenDateTime").html(times.krakenTime);
             });
         });
 
         var geminiCall = callExchangeApi('GET', 'https://api.gemini.com/v1/pubticker/btcusd', 
             'json', geminiSuccess, geminiError);
         geminiCall.always(function(){
-            $("#geminiBuy").html(geminiBuy);
-            $("#geminiDateTime").html(geminiTime);
+            $("#geminiBuy").html(buyPrices.geminiBuy);
+            $("#geminiDateTime").html(times.geminiTime);
         });
 
         var zebpayCall = callExchangeApi('GET', 'https://www.zebapi.com/api/v1/market/ticker/btc/inr', 
             'json', zebpaySuccess, zebpayError);
         zebpayCall.always(function(){
-            $("#zebpaySell").html(zebpaySell);
-            $("#zebpayDateTime").html(zebpayTime);
+            $("#zebpaySell").html(sellPrices.zebpaySell);
+            $("#zebpayDateTime").html(times.zebpayTime);
         });
 
         var unocoinCall = callExchangeApi('GET', 'https://www.unocoin.com/trade?sell', 
             '', unocoinSuccess, unocoinError);
         unocoinCall.always(function(){
-            $("#unocoinSell").html(unocoinSell);
-            $("#unocoinDateTime").html(unocoinTime);
+            $("#unocoinSell").html(sellPrices.unocoinSell);
+            $("#unocoinDateTime").html(times.unocoinTime);
         });
         
         var coinsecureCall = callExchangeApi('GET', 'https://api.coinsecure.in/v1/exchange/bid/high', 
             '', coinsecureSuccess, coinsecureError);
         coinsecureCall.always(function(){
-            $("#coinsecureSell").html(coinsecureSell);
-            $("#coinsecureDateTime").html(coinsecureTime);
+            $("#coinsecureSell").html(sellPrices.coinsecureSell);
+            $("#coinsecureDateTime").html(times.coinsecureTime);
         });
 
     }, 120);
